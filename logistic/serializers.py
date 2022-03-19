9ae880ee4078e11b531a4bdbb2d34aa0c3c7120f
Gunner539 <85799069+Gunner539@users.noreply.github.com>
2022-03-19 22:11:48 +0300

    Initial commit

---
+++ logistic/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+
+from logistic.models import Product, Stock, StockProduct
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    # настройте сериализатор для позиции продукта на складе
+    class Meta:
+        model = StockProduct
+        fields = ('id', 'product', 'quantity', 'price')
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+
+    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ('id', 'address', 'positions')
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # создаем склад по его параметрам
+        stock = Stock.objects.create(**validated_data)
+
+
+        # здесь вам надо заполнить связанные таблицы
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
+        for position in positions:
+            StockProduct.objects.create(stock=stock, **position)
+
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+        stock = super().update(instance, validated_data)
+        for position in positions:
+            items = StockProduct.objects.filter(stock=stock)
+            for item in items:
+                if item.product == position.get('product'):
+                    item.quantity = position.get('quantity')
+                    item.price = position.get('price')
+                    item.save()
+        return stock
